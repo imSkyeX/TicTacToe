5efce5b146586e2b3ebd9a8a1568d015ab2fd551
Pablo <pablo.sanchi.herrera@gmail.com>
2023-02-04 00:50:07 +0100

    update footer developers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ import '../styles/footer.css'
 const Footer = () => {
   return (
     <div className='footer-container'>
-        <div className='footer-text'>
-            <span>Open source TicTacToe.</span><br /><br />
-            This project was developed using <span>React.JS </span> 
-            and is licensed under the <span>MIT license. </span><br /><br />
-            Developers:
-            <ul>
-                <li>Pablo Sanchidrian</li>
-                <li>Jose Manuel Macias</li>
-            </ul>
-        </div>
+      <div className='footer-text'>
+        <span>Open source TicTacToe.</span><br /><br />
+        This project was developed using <span>React.JS </span> 
+        and is licensed under the <span>MIT license. </span><br /><br />
+        Developers:
+        <ul>
+          <li><a target="_blank" href='https://github.com/PabloSanchi'>PabloSanchi</a></li>
+          <li><a target="_blank" href='https://github.com/imSkyeX'>imSkyeX</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
